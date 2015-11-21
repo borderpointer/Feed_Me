e688cad10047030e0c98a7837c0f8efb6603d86c
borderpointer <ellijpark@gmail.com>
2015-11-21 14:03:48 -0500

    started working on the login/signup functions in app.js

--- feed-me-app/public/app.js
+++ feed-me-app/public/app.js
@@ -1,1 +1,55 @@
-console.log("linked");
+var user = null;
+var order = null;
+
+
+$(function() {
+
+    startApp();
+
+});
+
+var startApp = function() {
+
+    if (Cookies.get('loggedInUser') !== undefined) {
+
+        console.log("cookie present");
+
+    } else {
+
+        console.log("no cookies");
+
+        $('#header').append("<a href='#' id = 'login'>Login</a>");
+        $('#header').append("<a href='#' id = 'signup'>Create Account</a>");
+
+        invokeLoginForm();
+        //invokeSignupForm();
+
+    }
+
+}
+
+var invokeLoginForm = function() {
+
+    var $loginBtn = $('#login');
+
+    $loginBtn.click(function() {
+
+        showLoginForm();
+
+    });
+
+}
+
+var showLoginForm = function() {
+
+    var $template = Handlebars.compile($('#log-in-screen').html());
+
+    $('#container').append($template);
+
+    loginUser();
+
+}
+
+var loginUser = function() {
+
+}
